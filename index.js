4d32b2d7fff3f923b9dae02800c8c9304a402a04
yuu0252 <yutanmaron@gmail.com>
2024-02-16 20:09:04 +0900

    created USER CREATE method

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = 8000;
 require("dotenv").config();
 
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+const User = require("./src/v1/models/user");
+
 app.use(express.json());
 
 // DB接続
@@ -15,8 +19,20 @@ try {
   console.log(err);
 }
 
-app.get("/", (req, res) => {
-  return res.status(200).json("GET");
+// ユーザ新規登録
+app.post("/register", async (req, res) => {
+  const password = req.body.password;
+  try {
+    // パスワードを暗号化してユーザとトークンを返す
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    const user = await User.create(req.body);
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 app.listen(PORT, () => {
