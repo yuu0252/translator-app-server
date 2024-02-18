ddb09073c5301be1bb349ed32f415df266990719
yuu0252 <yutanmaron@gmail.com>
2024-02-19 02:57:21 +0900

    create Edit Pharase Method

--- src/v1/controller/phrase.ts
+++ src/v1/controller/phrase.ts
@@ -1,15 +1,51 @@
 import { Response } from 'express';
 import { Phrase } from '../models/Phrase';
 
-// フレーズの新規作成
-export const createPhrase = async (req: any, res: Response) => {
-  try {
-    const phrase = await Phrase.create({
-      user: req.user._id,
-      title: req.body.title,
-    });
-    res.status(201).json(phrase);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+export const phrase = {
+  // フレーズの新規作成
+  create: async (req: any, res: Response) => {
+    const { title } = req.body;
+    try {
+      if (title.replace(/\r?\n/g, '') === '')
+        return res.status(400).json('タイトルは必須項目です');
+
+      const phrase = await Phrase.create({
+        user: req.user._id,
+        title: req.body.title,
+      });
+      res.status(201).json(phrase);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  getAll: async (req: any, res: Response) => {
+    try {
+      const phrases = await Phrase.find({ user: req.user._id });
+      res.status(200).json(phrases);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  update: async (req: any, res: Response) => {
+    const { phraseId } = req.params;
+    const { title } = req.body;
+    try {
+      if (title.replace(/\r?\n/g, '') === '')
+        return res.status(400).json('タイトルは必須項目です');
+
+      const phrase = await Phrase.findOne({
+        user: req.user._id,
+        _id: phraseId,
+      });
+      if (!phrase) return res.status(404).json('フレーズが存在しません');
+
+      const updatedPhrase = await Phrase.findByIdAndUpdate(phraseId, {
+        $set: req.body,
+      });
+
+      res.status(200).json(updatedPhrase);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
--- src/v1/routes/phrase.ts
+++ src/v1/routes/phrase.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { verifyToken } from '../middleWare/tokenHandler';
-import { createPhrase } from '../controller/phrase';
+import { phrase } from '../controller/phrase';
 
 const router = Router();
 
 // フレーズの新規作成
-router.post('/', verifyToken, createPhrase);
+router.post('/', verifyToken, phrase.create);
+// ユーザのフレーズを取得
+router.get('/', verifyToken, phrase.getAll);
+// フレーズを編集
+router.put('/:phraseId', verifyToken, phrase.update);
 
 module.exports = router;
