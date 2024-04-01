bed11e77682ac783c9f32c3186d189685132c88e
yuu0252 <yutanmaron@gmail.com>
2024-04-01 11:28:07 +0900

    deploy GCR

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install --only=production
 COPY . ./
+ENV PORT 8080
 EXPOSE 8080
 CMD ["npm", "start"]
--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://multi-translator.com"],
   })
 );
 
